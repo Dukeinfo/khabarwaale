1a181f505f6529f98ad05954bf994842b1b86b4b
Amankhalsa <amansin31@gmail.com>
2023-11-15 00:43:16 +0530

    profile updated

--- resources/views/livewire/backend/settings/admin-profile.blade.php
+++ resources/views/livewire/backend/settings/admin-profile.blade.php
@@ -6,12 +6,12 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Admin Profile</h4>
+                        <h4 class="mb-sm-0 font-size-18"> Profile</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="{{url('admin/dashboard')}}">Dashboard</a></li>
-                                <li class="breadcrumb-item active">Admin Profile</li>
+                                <li class="breadcrumb-item active"> Profile</li>
                             </ol>
                         </div>
 
@@ -24,7 +24,7 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header bg-transparent border-bottom py-3">
-                            <h4 class="card-title">update Admin Profile</h4>
+                            <h4 class="card-title">update  Profile</h4>
                         </div>
                         <div class="card-body">
            
@@ -80,7 +80,7 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header bg-transparent border-bottom py-3">
-                            <h4 class="card-title">Update Admin Password</h4>
+                            <h4 class="card-title">Update  Password</h4>
                         </div>
                         <div class="card-body">
                         
