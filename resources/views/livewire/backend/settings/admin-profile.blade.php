dc83aabe1e289c525873f789c785df5d3b7c6ade
Amankhalsa <amansin31@gmail.com>
2023-11-15 01:02:45 +0530

    Auth Profile added

--- resources/views/livewire/backend/settings/admin-profile.blade.php
+++ resources/views/livewire/backend/settings/admin-profile.blade.php
@@ -6,12 +6,23 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18"> Profile</h4>
+                        @role('admin')
+                        <h4 class="mb-sm-0 font-size-18"> Admin Profile</h4>
+
+                        @else
+                        <h4 class="mb-sm-0 font-size-18"> 	{{ ucwords( auth()->user()->roles->pluck('name')[0] )?? '' }} Profile</h4>
+                        @endrole
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="{{url('admin/dashboard')}}">Dashboard</a></li>
-                                <li class="breadcrumb-item active"> Profile</li>
+                             @role('admin')
+
+                                <li class="breadcrumb-item active">Admin Profile</li>
+                                @else
+                                <li class="breadcrumb-item active">{{ ucwords( auth()->user()->roles->pluck('name')[0] )?? '' }} Profile</li>
+
+                                @endrole
                             </ol>
                         </div>
 
@@ -24,7 +35,16 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header bg-transparent border-bottom py-3">
+
+                            @role('admin')
+
                             <h4 class="card-title">update  Profile</h4>
+
+                            @else
+                            <h4 class="card-title">Update {{ ucwords( auth()->user()->roles->pluck('name')[0] )?? '' }}  Profile</h4>
+
+
+                            @endrole
                         </div>
                         <div class="card-body">
            
