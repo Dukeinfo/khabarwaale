1aed1ecbcda610856002abcb933448f2cbdbcd68
Amankhalsa <amansin31@gmail.com>
2023-12-11 23:27:50 +0530

    og image added now

--- resources/views/livewire/frontend/innerpage/view-inner-page.blade.php
+++ resources/views/livewire/frontend/innerpage/view-inner-page.blade.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
         <meta property="og:type" content="website"> 
         <meta property="og:title" content="{{ $getNewsDetail->title }}" />   
         <meta property="og:description" content="{{  strip_tags(Str::limit($getNewsDetail->news_description, 200)) ?? "NA" }}" />
-        <meta property="og:image" content="{{ $getNewsDetail->image_url }}" />
+        <meta property="og:image" content="{{   getNewsImage($getNewsDetail->image)  }}" />
         <meta property="og:image:width" content="1200"> <!-- Width of the image in pixels -->
         <meta property="og:image:height" content="630"> <!-- Height of the image in pixels -->
         <meta property="og:site_name" content="khabarwaale">
