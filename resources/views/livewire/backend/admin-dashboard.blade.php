131c3058697b30e3c4238452288e614cd5428381
Amankhalsa <amansin31@gmail.com>
2024-02-01 01:42:18 +0530

    Changes in dash

--- resources/views/livewire/backend/admin-dashboard.blade.php
+++ resources/views/livewire/backend/admin-dashboard.blade.php
@@ -19,10 +19,14 @@
                 </div>
             </div>
             <div class="row">
+                @php
+                $colors = ['bg-primary', 'bg-success', 'bg-danger', 'bg-warning', 'bg-info']; // Define colors
+                $colorIndex = 0; // Index for selecting colors
+            @endphp
                 @foreach ($totalyearCounts as $yearCount)
                 <div class="col-lg-4">
-                    <div class="card mb-4">
-                        <div class="card-header bg-primary text-white">
+                    <div class="card mb-4 ">
+                        <div class="card-header  {{ $colors[$colorIndex % count($colors)] }} text-white">
                             <h5 class="card-title">{{ $yearCount->year }}</h5>
                         </div>
                         <div class="card-body">
@@ -30,6 +34,9 @@
                         </div>
                     </div>
                 </div>
+                @php
+                $colorIndex++; // Move to the next color
+            @endphp
                 @endforeach
             </div>
             @php
@@ -59,8 +66,14 @@
                             <h4 class="card-title">Monthly Counts</h4>
                         </div>
                         <div class="card-body">
+                            @php
+                            $colors = ['bg-primary', 'bg-success', 'bg-danger', 'bg-warning', 'bg-info']; // Define colors
+                            $colorIndex = 0; // Index for selecting colors
+                        @endphp
                             @foreach ($modifiedCounts as $year => $months)
-                                <h5>{{ $year }}</h5>
+                       
+                        
+                                <h5 class=" {{ $colors[$colorIndex % count($colors)] }} text-white px-3 py-2">{{ $year }}</h5>
                                 <div class="table-responsive">
                                     <table class="table table-striped">
                                     <thead>
@@ -70,7 +83,9 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                 
+                                 @php
+                                      $colorIndex++; // Move to the next color
+                                 @endphp
                                         @foreach ($months as $month => $count)
                                             <tr>
                                                 <td>{{ \Carbon\Carbon::create()->month($month)->format('F') }}</td>
